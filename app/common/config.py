a1effab23251593dbc64eb2026b0121a906611e9
sotaireange <sotaireange@gmail.com>
2025-05-11 15:06:45 +0600

    add worker and upgrade all app/db/models

--- app/common/config.py
+++ app/common/config.py
@@ -33,7 +33,7 @@ class Settings(BaseSettings):
     DB_HOST: str = "localhost"
     DB_PORT: int = 5432
     DB_USER: str = "postgres"
-    DB_PASSWORD: SecretStr ='0880'
+    POSTGRES_PASSWORD: SecretStr ='0880'
     DB_NAME: str = "bybit_test"
 
 
