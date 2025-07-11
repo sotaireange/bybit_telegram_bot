6aa66f4e33b6fc21617d18384321db59da9beb44
sotaireange <sotaireange@gmail.com>
2025-07-11 17:06:46 +0600

    Prepare to next stage.

--- app/telegram/handlers/user/run.py
+++ app/telegram/handlers/user/run.py
@@ -154,9 +154,9 @@ async def signal(message: Message, redis_client: RedisClient, user: User):
             data[coin]={"Long": bool(buy),"Short": not bool(buy)}
     except:
         text= msg("error_when_add_coin")
-        await message.bot.send_message(chat_id=msg.chat.id,text=text)
+        await message.bot.send_message(chat_id=message.from_user.id,text=text)
         return
 
     await redis_client.save_coin_by_user(data,user_id)
     text=msg.get_coins_add_text(data)
-    await message.bot.send_message(chat_id=msg.chat.id,text=text)
+    await message.bot.send_message(chat_id=message.from_user.id,text=text)
